c6e4715a175ea01e8db1210c351ca8b7e4d339e3
meteis <meteis.1@qq.com>
2020-09-02 13:53:58 +0800

    update gitee.user.js.

--- gitee.user.js
+++ gitee.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Gitee
 // @namespace    
-// @version      0.0.1
+// @version      0.0.2
 // @description   
 // @supportURL   
 // @match        https://gitee.com/*
