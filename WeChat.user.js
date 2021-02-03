cf5e80e0a1677e163a79b7f023efee6d094fdb18
sonamu <sonamu@foxmail.com>
2021-02-03 09:37:11 +0800

    update WeChat.user.js.

--- WeChat.user.js
+++ WeChat.user.js
@@ -25,6 +25,7 @@ if(mainDiv && mainDiv.length > 0){
 
 let c141 = '#141414';
 let cfa6 = '#ffffffa6';
+let c808 = '#808080';
 
 // custom style
 let styStr = 
@@ -42,12 +43,12 @@ let styStr =
   + '.box_hd .title .title_name {color: #ffffffa6;font-weight: 100;width: 10px;font-size:10px;}'
 // 编辑栏
   + '.chat .box_ft {height: 180px;margin-right: 19px;border-top: 1px solid #ffffff1f;border-top-color: #ffffff1f;}'
-	+ '.chat .box_ft .content .flex { font-size: 10px; color: '+ cfa6 +'; }'
+	+ '.chat .box_ft .content .flex { font-size: 10px; color: '+ c808 +'; }'
 	+ '.chat .box_ft .desc {display: none;}'
 // 主消息
-	+ '.bubble.bubble_primary {background-color: '+ c141 +';color: '+ cfa6 +';}'
+	+ '.bubble.bubble_primary {background-color: '+ c141 +';color: #808080;}'
   + '.bubble.bubble_primary.right.arrow_primary::before, .bubble.bubble_primary.right::after {border: solid 3px #177ddc;background: #177ddc;border-radius: 100%;}'
-	+ '.bubble.bubble_default {background-color: #272825;color: #808080;}'
+	+ '.bubble.bubble_default {background-color: #141414;color: #808080;}'
   + '.bubble.left::after {border-right-color: #141414;}'
   + '.message.me {text-align: left;}'
   + '.bubble.right::after, .bubble.right::before {left: 0;}'
