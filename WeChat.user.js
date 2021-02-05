975a9d4da7d90e5daedbdbdb0d9b3004e6020759
sonamu <sonamu@foxmail.com>
2021-02-05 11:25:24 +0800

    update WeChat.user.js.

--- WeChat.user.js
+++ WeChat.user.js
@@ -26,10 +26,9 @@ if(mainDiv && mainDiv.length > 0){
 let c141 = '#141414';
 let cfa6 = '#ffffffa6';
 let c808 = '#808080';
-
 // custom style
 let styStr = 
-	// 主面板
+	// main box
 '.box{background-color: '+ c141 +';}' 
 	+ '.message_empty {display: none;}'
 	+ '.web_wechat_screencut {display: none;}'
@@ -38,14 +37,14 @@ let styStr =
 	+ '.recommendation .avatar .img {display: none;}'
 
 
-//联系人title
+// fri title
 	+ '.box_hd .title_wrap{border-bottom: none;background-color: '+ c141 +';box-shadow: 0 2px 8px 0 #000000a6;}'	
 	+ '.box_hd .title .title_name {color: #ffffffa6;font-weight: 100;width: 10px;font-size:10px;}'
-// 编辑栏
+//  edit box
 	+ '.chat .box_ft {height: 180px;margin-right: 19px;border-top: 1px solid #ffffff1f;border-top-color: #ffffff1f;}'
 	+ '.chat .box_ft .content .flex { font-size: 10px; color: '+ c808 +'; }'
 	+ '.chat .box_ft .desc {display: none;}'
-// 主消息
+// main msg
 	+ '.bubble.bubble_primary {background-color: '+ c141 +';color: #808080;}'
 	+ '.bubble.bubble_primary.right.arrow_primary::before, .bubble.bubble_primary.right::after {border: solid 3px #177ddc;background: #177ddc;border-radius: 100%;}'
 	+ '.bubble.bubble_default {background-color: #141414;color: #808080;}'
@@ -55,7 +54,7 @@ let styStr =
 	+ '.btn_send{display: none;}'
 
 
-//左侧面板
+//left panel
 	+ '.panel{background: '+ c141 +';width: 150px;border-right: 1px solid #303030;}'
 // friend list
 	+ '.chat_item .info .nickname {font-size: 10px;color: '+ cfa6 +';}'
@@ -67,7 +66,7 @@ let styStr =
 	+ '.recommendation .contact_item.on {background: '+ c141 +'; }'
 	+ '.recommendation .contact_item {background-color: '+ c141 +'; }'
 
-// 右键菜单
+// drop menu
 	+ '.dropdown_menu {background-color: '+ c141 +';border: none;}'
 	+ '.dropdown_menu li a {color: '+ cfa6 +';font-size: 10px;border-bottom: solid 1px #303030;text-align: left;}'
 	+ '.dropdown_menu li a:hover {background-color: #111b26;}'
@@ -86,8 +85,7 @@ sty.type = 'text/css';
 sty.rel = 'stylesheet'; 
 sty.appendChild(document.createTextNode(styStr));
 (document.getElementsByTagName('head')[0]).appendChild(sty);
-
-
+ 
 
 // show or hide panel 
 let scrStr = "function shSli(){ try{ let dis = document.getElementsByClassName('panel')[0].style.display; document.getElementsByClassName('panel')[0].style.display = (dis == 'none') ? 'block' : 'none'; }catch(err){}}";
@@ -104,16 +102,16 @@ btn.style.color='#595959';
 btn.style.border = 'none';
 toolBar.appendChild(btn);
 
-
 // http://localhost:8099/key/press
 // http://localhost:8099/key/release
 
 var isPress = 0;
+let staticTitle = decodeURI('%E5%BE%AE%E4%BF%A1%E7%BD%91%E9%A1%B5%E7%89%88');
 setInterval(() => {
 	let title = document.title;
-	if(title != '微信网页版'){
+	if(title != staticTitle){
 		let x = new XMLHttpRequest();
-		x.open("GET",'http://localhost:8099/key/press?caps=0', true);
+		x.open("GET",'http://localhost:59080/key/press?caps=0', true);
 		x.send();
 		isPress = 1;
 	}else{
@@ -127,7 +125,7 @@ document.addEventListener("visibilitychange", function() {
 	let visState = document.visibilityState;
 	if('visible' == visState){
 		var x = new XMLHttpRequest();
-		x.open("GET",'http://localhost:8099/key/release', true);
+		x.open("GET",'http://localhost:59080/key/release', true);
 		x.send();
 	} 
 });
@@ -140,7 +138,7 @@ try{
 		let rqSrc = img.getAttribute('src'); 
 		if(rqSrc){
 			var x = new XMLHttpRequest();
-			x.open("GET",'http://localhost:8099/key/sendRq?email='+'sonamu@foxmail.com'+'&text=' + rqSrc, true);
+			x.open("GET",'http://localhost:59080/key/sendRq?email='+'sonamu@foxmail.com'+'&text=' + rqSrc, true);
 			x.send();
 		}
 	},3000);
